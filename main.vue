c491e3540064b9f425cb3986d9dbd2ca7efbeb13
Learning <liangyongning@gmail.com>
2019-06-21 17:33:16 +0800

    init repo

---
+++ main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <a href="#" @click.prevent="triggle">Triggle</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      triggle (e) {
+        console.log(e)
+      }
+    }
+  }
+</script>
